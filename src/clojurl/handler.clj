ba64cba8d17f8c17eee161d96f1987efada5144a
Neil Lambert <neil.lambert@fundingcircle.com>
2016-12-24 14:19:50 -0800

    handler + tests

--- src/clojurl/handler.clj
+++ src/clojurl/handler.clj
@@ -3,10 +3,31 @@
             [ring.util.response :as response]
             [clojurl.storage :as st]))
 
-(defn get-link
+(defn handle-get-link
   [stg id]
   (if-let [url (st/get-link stg id)]
     (response/redirect url)
-    (response/not-found "Sorry, that's not a valid link")))
+    (response/not-found "Sorry, that's not a valid link.")))
 
+(defn handle-create-link
+  [stg id url]
+  (if (st/create-link stg id url)
+    (-> (response/response id)
+        (response/status 201))
+    (response/not-found "Sorry, link already taken.")))
 
+(defn handle-update-link
+  [stg id new-url]
+  (if (st/update-link stg id new-url)
+    (response/response id)
+    (response/not-found "No stored link with this id.") ))
+
+(defn handle-delete-link
+  [stg id]
+  (if (st/delete-link stg id)
+    (response/response id)
+    (response/not-found "No stored link with this id.") ))
+
+(defn handle-list-links
+  [stg]
+  (response/response (st/list-links stg)))
--- test/clojurl/handler_test.clj
+++ test/clojurl/handler_test.clj
@@ -10,9 +10,70 @@
         id "test"
         url "http://www.test.com"]
     (st/create-link stg id url)
+
     (testing "when ID exists"
-      (let [response (get-link stg id)]
-        (is (= 302 (:status response)))))
+      (let [response (handle-get-link stg id)]
+        (is (= 302 (:status response)))
+        (is (= url (get-in response [:headers "Location"])))))
+
     (testing "when ID does not exist"
       (let [response (get-link stg "foo")]
         (is (= 404 (:status response)))))))
+
+(deftest create-link-test
+  (let [stg (in-memory-storage)
+        id "goog"
+        url "http://www.google.com"]
+    (testing "returns the id in the response"
+      (let [response (handle-create-link stg id url)]
+        (is (= 201 (:status response)))
+        (is (= id (:body response)))))))
+
+(deftest update-link-test
+  (let [stg (in-memory-storage)
+        id "goog"
+        url "http://www.google.com"
+        new-url "https://www.googs.com"]
+    (st/create-link stg id url)
+    (testing "returns 200 when successful"
+      (let [response (handle-update-link stg id new-url)]
+        (is (= 200 (:status response)))
+        (is (= id (:body response)))))
+    (testing "returns a 400 when updating a link that doesn't exist"
+      (let [response (handle-update-link stg "bogus" new-url)]
+        (is (= 404 (:status response)))
+        (is (= "No stored link with this id." (:body response)))) )))
+
+
+(deftest delete-link-test
+  (let [stg (in-memory-storage)
+        id "goog"
+        url "http://www.google.com"
+        new-url "https://www.googs.com"]
+    (st/create-link stg id url)
+
+    (testing "deletes a link if it exists"
+      (let [response (handle-delete-link stg id)]
+        (is (= 200 (:status response)))
+        (is (= 404 (:status (handle-get-link stg id))))))
+
+    (testing "returns 404 if there's no link stored with given id"
+      (let [response (handle-delete-link stg "bogus")]
+        (is (= 404 (:status response)))
+        (is (= "No stored link with this id." (:body response)))))))
+
+(deftest list-links-test
+  (let [stg (in-memory-storage)
+        links {"foo" "www.foobar.com"
+               "gmail" "www.gmail.com"
+               "yahoo" "www.isyahoodeadyet.com"}]
+
+    (st/create-link stg "foo" "www.foobar.com")
+    (st/create-link stg "gmail" "www.gmail.com")
+    (st/create-link stg "yahoo" "www.isyahoodeadyet.com")
+
+    (testing "returns a list of stored links"
+      (let [response (handle-list-links stg)]
+        (is (= 200 (:status response)))
+        (is (= links (:body response)))))))
+
